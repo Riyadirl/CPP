0d1123850218aba35d97fe9bb71a92086a431a37
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-06-01 22:44:33 +0600

    .

---
+++ week-2/m3/3_selection_sort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+    // assending
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i = 1; j < n; j++)
+        {
+            if (a[i].marks > a[j].marks)
+            {
+                swap(a[i], a[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+
+    // highest mark & lowest roll
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i = 1; j < n; j++)
+        {
+            if (a[i].marks < a[j].marks)
+            {
+                swap(a[i], a[j]);
+            }
+            if (a[i].roll == a[j].roll)
+            {
+                if (a[i].roll > a[j].roll)
+                {
+                    swap(a[i], a[j]);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+
+    return 0;
+}
